1023e86bd04e2612b31b99b5cb09035331f94cf2
Fabricio <fabricio.beretta@acad.pucrs.br>
2020-04-10 21:13:26 -0300

    Inclusão de Calculo Simples no HTML

---
+++ Bitcoin_TCC/Models/CalcularBitcoin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class CalcularBitcoin
+{
+    /*public CalcularBitcoin()
+    {
+        double num1, num2;
+
+
+        //var num1 = Number(document.getElementById("num1").value);
+
+
+
+
+    }*/
+}
