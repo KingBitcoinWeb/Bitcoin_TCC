bb82251f1e9f6da30ef814fd1ce50ad1c95e7d2d
Fabricio <fabricio.beretta@acad.pucrs.br>
2020-04-21 21:03:03 -0300

    Inclusão do Banco de Dados

---
+++ Bitcoin_TCC/Models/ConexaoBancoDados.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitcoin_TCC.Models
+{
+    public class ConexaoBancoDados
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "kingbitcoindb.database.windows.net";
+                builder.UserID = "kingbitcoinweb";
+                builder.Password = "BitcoinWeb@3000";
+                builder.InitialCatalog = "KingBitcoindb";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("INSERT INTO contatobitcoin VALUES ('Fabricio Teste', 'Email@teste.com.br', 'Conexao Banco de dados')");
+                    //sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+                    //sb.Append("JOIN [SalesLT].[Product] p ");
+                    //sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
